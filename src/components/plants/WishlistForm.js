7fffd9a3419a57644fcc4569ab176b493a7e5542
Tifana Rosa <tifanarosa@gmail.com>
2021-09-03 15:31:27 +0200

    adding firebase and event listener for form

--- src/components/plants/WishlistForm.js
+++ src/components/plants/WishlistForm.js
@@ -1,25 +1,52 @@
 import classes from "./WishlistForm.module.css";
 import Card from "../ui/Card";
+import { useRef } from "react";
 
-function WishlistForm() {
+function WishlistForm(props) {
+  //for reading the entered values, setting up our own references/connection to DOM elements
+  const nameInputRef = useRef();
+  const imageInputRef = useRef();
+  const typeInputRef = useRef();
+  const descriptionInputRef = useRef();
+
+  function submitHandler(event) {
+    event.preventDefault();
+    
+    const enteredName = nameInputRef.current.value;
+    const enteredImage = imageInputRef.current.value;
+    const enteredType = typeInputRef.current.value;
+    const enteredDescription = descriptionInputRef.current.value;
+    
+    //creating a new object to store the values
+    const plantData ={
+      name: enteredName,
+      image: enteredImage,
+      type: enteredType,
+      description: enteredDescription
+    }
+
+    props.onAddPlant(plantData)
+
+  }
+
   return (
     <Card>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor="name">Name</label>
-          <input type="text" required id="name" />
+          <input type="text" required id="name" ref={nameInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor="image">Plant Image</label>
-          <input type="url" required id="image" />
+          <input type="url" required id="image" ref={imageInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor="type">Type</label>
-          <input type="text" required id="type" />
+          <input type="text" required id="type" ref={typeInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor="description">Description</label>
-          <textarea id="description" required rows="5"></textarea>
+          <textarea id="description" required rows="5" ref={descriptionInputRef}></textarea>
         </div>
         <div className={classes.actions}>
           <button>Add Plant</button>
--- src/pages/Wishlist.js
+++ src/pages/Wishlist.js
@@ -1,11 +1,28 @@
 import WishlistForm from "../components/plants/WishlistForm";
 
 function Wishlist() {
-    return <section>
+  function addPlantHandler(plantData) {
+    //firebase require .json
+    fetch(
+      "https://plantcoolections-default-rtdb.firebaseio.com/plants.json",
+      //to store data to firebase we must send post request'
+        {
+          method: 'POST',
+          body: JSON.stringify(plantData),
+          headers: {
+            'Content-Type': 'application/json'
+          } 
+        }
+    );
+  }
+
+  return (
+    <section>
       <h1>Wishlist</h1>
       <h1>Add your wish plant here!</h1>
-      <WishlistForm />
+      <WishlistForm onAddPlant={addPlantHandler} />
     </section>
-  }
-  
-  export default Wishlist;
+  );
+}
+
+export default Wishlist;
