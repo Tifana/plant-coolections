702641e664646ae2b598d8bfaf33e77bd36efcbe
Tifana Rosa <tifanarosa@gmail.com>
2021-09-05 02:51:12 +0200

    navigating data and display on the page

--- src/pages/Wishlist.js
+++ src/pages/Wishlist.js
@@ -1,6 +1,11 @@
+import { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
 import WishlistForm from "../components/plants/WishlistForm";
+import PlantList from "../components/plants/PlantList";
 
 function Wishlist() {
+  const history = useHistory(); //to navigate the input after we get it
+
   function addPlantHandler(plantData) {
     //firebase require .json
     fetch(
@@ -13,7 +18,45 @@ function Wishlist() {
             'Content-Type': 'application/json'
           } 
         }
-    );
+    ).then(() => {
+      history.replace('/MyPlants')
+    })
+  }
+
+  const [isLoading, setIsloading] = useState(true)
+  const [loadedPlants, setLoadedPlants] = useState([])
+
+  useEffect(() => {
+    setIsloading(true)
+    fetch(
+      "https://plantcoolections-default-rtdb.firebaseio.com/plants.json" 
+    )
+    .then((response) => {
+      return response.json()
+    })
+    .then((data) => {
+      const plants = []
+
+      for (const key in data) {
+        const plant = {
+          id: key,
+          ...data[key]
+        }
+
+        plants.push(plant)
+      }
+
+      setIsloading(false)
+      setLoadedPlants(plants)
+    })
+  }, [])
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading ... </p>
+      </section>
+    )
   }
 
   return (
@@ -21,6 +64,7 @@ function Wishlist() {
       <h1>Wishlist</h1>
       <h1>Add your wish plant here!</h1>
       <WishlistForm onAddPlant={addPlantHandler} />
+      <PlantList plants={loadedPlants} />
     </section>
   );
 }
